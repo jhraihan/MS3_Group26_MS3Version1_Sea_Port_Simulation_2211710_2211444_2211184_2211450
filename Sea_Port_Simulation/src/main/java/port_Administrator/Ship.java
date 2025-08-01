c21176acd6c0a56beb46c4ae764ffd77bf94f201
jhraihan <jahidhr05@gmail.com>
2025-08-01 20:56:54 +0600

    added ship

---
+++ Sea_Port_Simulation/src/main/java/port_Administrator/Ship.java
@@ -0,0 +1,9 @@
+package port_Administrator;
+
+public class Ship {
+    private String ship_id;
+    private String name;
+    private String Cargo_type;
+    private String size;
+    private String assigned_dock;
+}
