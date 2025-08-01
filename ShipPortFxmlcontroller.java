7a20b8a779af2f7f0c2cb2c7a8ca2ab93f45bd6c
Mary Chowdhury <maryrhym@gmail.com>
2025-08-02 02:32:32 +0600

    Add files via upload

---
+++ ShipPortFxmlcontroller.java
@@ -0,0 +1,101 @@
+package com.example.shipport;
+
+public class ShipPortFxmlcontroller
+{
+    @javafx.fxml.FXML
+    private TextField tfGateNumber;
+    @javafx.fxml.FXML
+    private TableColumn tvGateNoCol;
+    @javafx.fxml.FXML
+    private TextField tfDateFilter;
+    @javafx.fxml.FXML
+    private TableColumn tvCrewIdCol;
+    @javafx.fxml.FXML
+    private TableColumn tvShipIdCol;
+    @javafx.fxml.FXML
+    private TableColumn tvStatusCol;
+    @javafx.fxml.FXML
+    private TableColumn tvTimeCol;
+    @javafx.fxml.FXML
+    private TextField tfTransactionId;
+    @javafx.fxml.FXML
+    private TableView tvTableColumns;
+    @javafx.fxml.FXML
+    private TextField tfCrewIds;
+    @javafx.fxml.FXML
+    private TextField tfTaxConfiguration;
+    @javafx.fxml.FXML
+    private Label lbIdentityStatus;
+    @javafx.fxml.FXML
+    private TableColumn tvAmountPaidCol;
+    @javafx.fxml.FXML
+    private TextField tfAmountPaid;
+    @javafx.fxml.FXML
+    private AnchorPane tfShipId;
+    @javafx.fxml.FXML
+    private TextField tfLogInId;
+    @javafx.fxml.FXML
+    private ComboBox cbCameraLocation;
+    @javafx.fxml.FXML
+    private TextField tfClientFilter;
+    @javafx.fxml.FXML
+    private TableColumn tvDateCol;
+    @javafx.fxml.FXML
+    private TableColumn tvClientNameCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void oaGenerateSecurityReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaAuditReport(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaViewOverdue(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaRestrictedZones(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaCategoryFilter(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaAccessPortLogbook(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaCargoVerification(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaCctvFootages(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaPayOnline(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaTariffs(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaGenerateInvoice(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaRevenueAnalytics(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void oaStatusFilter(ActionEvent actionEvent) {
+    }
+}
