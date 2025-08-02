0169212d84bb5e1f3580d4a1d9be80b497b4c903
Mary Chowdhury <maryrhym@gmail.com>
2025-08-02 20:52:43 +0600

    Delete module-info.java

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sea_port_securityofficer {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sea_port_securityofficer to javafx.fxml;
-    exports com.example.sea_port_securityofficer;
-}
