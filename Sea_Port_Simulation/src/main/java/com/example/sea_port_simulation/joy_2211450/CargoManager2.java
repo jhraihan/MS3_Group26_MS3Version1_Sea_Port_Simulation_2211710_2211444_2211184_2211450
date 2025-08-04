48e462718760f12fc056099c6e0f3b1de9c60db4
joy1010joy <shahin.joy.2020@gmail.com>
2025-08-04 21:47:40 +0600

    joy

--- Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/joy_2211450/CargoManager2.java
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/joy_2211450/CargoManager2.java
@@ -2,18 +2,15 @@ package com.example.sea_port_simulation.joy_2211450;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class CargoManager1 {
+public class CargoManager2 {
     @javafx.fxml.FXML
-    private TextField CargoTypeTextField;
+    private TextField DepTimeTextField;
     @javafx.fxml.FXML
-    private Label ConfiramationMessageLable;
+    private Button UpdateButton;
     @javafx.fxml.FXML
     private TextField CargoIdTextField;
     @javafx.fxml.FXML
-    private Button SubmitButton;
-    @javafx.fxml.FXML
-    private ComboBox ValidateComboBox;
+    private ComboBox ValidateTimeComboBox;
 }
