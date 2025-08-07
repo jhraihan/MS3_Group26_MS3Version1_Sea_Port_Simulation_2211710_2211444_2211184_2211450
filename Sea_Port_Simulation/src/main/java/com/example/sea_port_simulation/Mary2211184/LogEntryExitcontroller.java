e6f4096297a44679c15ff6314887bfd79eb3a9b2
mary-2211184 <maryrhym@gmail.com>
2025-08-08 03:33:35 +0600

    "Mary Commit"

---
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/Mary2211184/LogEntryExitcontroller.java
@@ -0,0 +1,22 @@
+package com.example.sea_port_simulation.Mary2211184;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LogEntryExitcontroller
+{
+    @javafx.fxml.FXML
+    private TextField txtShipName;
+    @javafx.fxml.FXML
+    private TextField txtCrewId;
+    @javafx.fxml.FXML
+    private TextField txtGateNumber;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRecordEntry(ActionEvent actionEvent) {
+    }
+}
