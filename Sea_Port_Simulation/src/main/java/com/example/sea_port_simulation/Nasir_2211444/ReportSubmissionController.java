adbc458e2e897c8a228327210c46f173028e5d6a
NRRatul55 <2211444@iub.edu.bd>
2025-08-14 21:22:56 +0600

    Nasir

--- Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/Nasir_2211444/ReportSubmissionController.java
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/Nasir_2211444/ReportSubmissionController.java
@@ -1,4 +1,4 @@
-package com.example.sea_port_simulation.Nasir;
+package com.example.sea_port_simulation.Nasir_2211444;
 
 public class ReportSubmissionController
 {
--- Sea_Port_Simulation/src/main/java/module-info.java
+++ Sea_Port_Simulation/src/main/java/module-info.java
@@ -7,8 +7,8 @@ module com.example.sea_port_simulation {
     exports com.example.sea_port_simulation;
     exports com.example.sea_port_simulation.joy_2211450;
     opens com.example.sea_port_simulation.joy_2211450 to javafx.fxml;
-    exports com.example.sea_port_simulation.Nasir;
-    opens com.example.sea_port_simulation.Nasir to javafx.fxml;
+    exports com.example.sea_port_simulation.Nasir_2211444;
+    opens com.example.sea_port_simulation.Nasir_2211444 to javafx.fxml;
     exports com.example.sea_port_simulation.jahid_2211710;
     opens com.example.sea_port_simulation.jahid_2211710 to javafx.fxml;
 
