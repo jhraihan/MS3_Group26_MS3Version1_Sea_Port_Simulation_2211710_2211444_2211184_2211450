bd6c6a68bd381e99a9cf5bdf42c5945913137ad4
jhraihan <jahidhr05@gmail.com>
2025-08-15 23:57:08 +0600

    jahid all ids done

--- Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/jahid_2211710/EmergencyAlertViewController.java
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/jahid_2211710/EmergencyAlertViewController.java
@@ -1,7 +1,38 @@
 package com.example.sea_port_simulation.jahid_2211710;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class EmergencyAlertViewController
 {
     @javafx.fxml.FXML
+    private TableColumn locationCol;
+    @javafx.fxml.FXML
+    private TableView emergencyAlertTableview;
+    @javafx.fxml.FXML
+    private ComboBox statusCombobox;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private TextArea descriptiontextarea;
+    @javafx.fxml.FXML
+    private TextField alertIDtextfield;
+    @javafx.fxml.FXML
+    private ComboBox alertTypecombobox;
+    @javafx.fxml.FXML
+    private TableColumn alertIDcol;
+    @javafx.fxml.FXML
+    private TextField locationTextfield;
+    @javafx.fxml.FXML
+    private TableColumn alertTypeCol;
+    @javafx.fxml.FXML
+    private TableColumn descriptionCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void createEmergencyButtonOnAction(ActionEvent actionEvent) {
+    }
+}
--- Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/jahid_2211710/UserAccountViewController.java
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/jahid_2211710/UserAccountViewController.java
@@ -1,7 +1,50 @@
 package com.example.sea_port_simulation.jahid_2211710;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class UserAccountViewController
 {
     @javafx.fxml.FXML
+    private TableColumn usernameCol;
+    @javafx.fxml.FXML
+    private TextField fullNametextfield;
+    @javafx.fxml.FXML
+    private TableColumn lastLoginCol;
+    @javafx.fxml.FXML
+    private TableColumn userIDcol;
+    @javafx.fxml.FXML
+    private TextField emailTextfield;
+    @javafx.fxml.FXML
+    private TextField usernametextfield;
+    @javafx.fxml.FXML
+    private TableColumn passwordCol;
+    @javafx.fxml.FXML
+    private TableColumn fullNamecol;
+    @javafx.fxml.FXML
+    private TextField userIDtextfield;
+    @javafx.fxml.FXML
+    private DatePicker lastLogINdatepicker;
+    @javafx.fxml.FXML
+    private ComboBox statusCombobox;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private TableView userAccountManagementTableview;
+    @javafx.fxml.FXML
+    private TextField passwordtextfield;
+    @javafx.fxml.FXML
+    private ComboBox selectRolecombobox;
+    @javafx.fxml.FXML
+    private TableColumn emailCol;
+    @javafx.fxml.FXML
+    private TableColumn roleCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void createUserButtonOnAction(ActionEvent actionEvent) {
+    }
+}
