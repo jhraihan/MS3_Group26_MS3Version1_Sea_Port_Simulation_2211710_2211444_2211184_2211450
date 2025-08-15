69d3ffffe9be4721791a5ac81268602fbae3820d
joy1010joy <shahin.joy.2020@gmail.com>
2025-08-16 00:59:57 +0600

    joy

--- Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/joy_2211450/CargoManager3.java
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/joy_2211450/CargoManager3.java
@@ -2,73 +2,35 @@ package com.example.sea_port_simulation.joy_2211450;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-<<<<<<< HEAD
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-=======
-import javafx.scene.control.*;
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CargoManager3 {
     @javafx.fxml.FXML
-<<<<<<< HEAD
     private TableColumn <CargoManager3,String>CargoIdCol;
     @javafx.fxml.FXML
-=======
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
     private TextField CargoIdTextField;
     @javafx.fxml.FXML
     private Label ConfirmationLable;
     @javafx.fxml.FXML
-<<<<<<< HEAD
-=======
-    private TableColumn<CargoManager3,String> CargoIdCol;
-    @javafx.fxml.FXML
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
     private TableColumn <CargoManager3,String>AvailableSlotCol;
     @javafx.fxml.FXML
     private TextField AvailableSlotTextField;
     @javafx.fxml.FXML
-    private TableView <CargoManager3>TableView;
+    private TableView<CargoManager3> TableView;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
     @FXML
     public void initialize() {
         CargoIdCol.setCellValueFactory(new PropertyValueFactory<>("CargoId"));
         AvailableSlotCol.setCellValueFactory(new PropertyValueFactory<>("AvailableSlot"));
-<<<<<<< HEAD
-    }
-=======
-        }
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
 
-    @FXML
+    }
+    @javafx.fxml.FXML
     public void AssignButton(ActionEvent actionEvent) {
-        String cargoId = CargoIdTextField.getText();
-        String AvailableSlot=AvailableSlotTextField.getText();
-        if (cargoId.isEmpty() || AvailableSlot.isEmpty()) {
-            ConfirmationLable.setText("Fill all boxes");
-            return;
-        }
-<<<<<<< HEAD
-
 
     }
 }
-
-
-
-=======
-        CargoManager1Model newCargo = new CargoManager1Model(cargoId,AvailableSlot );
-        TableView.getItems().add(newCargo);
-        ConfirmationLable.setText("successfully!");
-
-    }
-}
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f
--- Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/joy_2211450/CargoManager8.java
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/joy_2211450/CargoManager8.java
@@ -2,34 +2,13 @@ package com.example.sea_port_simulation.joy_2211450;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-<<<<<<< HEAD
 import javafx.scene.control.*;
-=======
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class CargoManager8 {
+
     @javafx.fxml.FXML
-<<<<<<< HEAD
     private TableColumn<CargoManager8,String> EnterRemarkCol;
-    @javafx.fxml.FXML
-    private TableColumn<CargoManager8,String> CargoIdCol;
-    @javafx.fxml.FXML
-    private TableColumn <CargoManager8,String>StatusChoiceBoxCol;
-    @javafx.fxml.FXML
-    private TextField CargoIdTextField;
-    @javafx.fxml.FXML
-    private ChoiceBox <String>StatusChoiceBox;
-    @javafx.fxml.FXML
-    private TextArea EnterRemarkTextArea;
-    @javafx.fxml.FXML
-    private TableView<CargoManager8> TableView;
-=======
-    private TableColumn <CargoManager8,String>EnterRemarkCol;
     @javafx.fxml.FXML
     private TableColumn <CargoManager8,String>CargoIdCol;
     @javafx.fxml.FXML
@@ -37,39 +16,23 @@ public class CargoManager8 {
     @javafx.fxml.FXML
     private TextField CargoIdTextField;
     @javafx.fxml.FXML
-    private ChoiceBox StatusChoiceBox;
+    private ChoiceBox <String>StatusChoiceBox;
     @javafx.fxml.FXML
     private TextArea EnterRemarkTextArea;
     @javafx.fxml.FXML
     private TableView <CargoManager8>TableView;
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
 
     @FXML
     public void initialize() {
         CargoIdCol.setCellValueFactory(new PropertyValueFactory<>("CargoId"));
         StatusChoiceBoxCol.setCellValueFactory(new PropertyValueFactory<>("Status"));
         EnterRemarkCol .setCellValueFactory(new PropertyValueFactory<>("EnterRemark"));
-<<<<<<< HEAD
 
 
     }
 
-        @javafx.fxml.FXML
-    public void UpdateButton(ActionEvent actionEvent) {
-            String CargoId=CargoIdTextField.getText();
-            String EnterRemark=EnterRemarkTextArea.getText();
-            String Status=StatusChoiceBoxCol.getText();
-
-
-
-        }
-}
-
-=======
-    }
 
     @javafx.fxml.FXML
     public void UpdateButton(ActionEvent actionEvent) {
     }
 }
->>>>>>> cff924a0e645e9d861be3e59aecc5f8f12c623b7
