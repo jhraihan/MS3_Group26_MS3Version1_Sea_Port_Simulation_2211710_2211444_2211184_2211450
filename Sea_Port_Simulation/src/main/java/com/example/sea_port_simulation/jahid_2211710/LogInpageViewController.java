f23c54ab3f9e55256388df0b27886c7ea4045cc3
jhraihan <jahidhr05@gmail.com>
2025-08-16 14:35:05 +0600

    login page controller

--- Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/jahid_2211710/LogInpageViewController.java
+++ Sea_Port_Simulation/src/main/java/com/example/sea_port_simulation/jahid_2211710/LogInpageViewController.java
@@ -1,7 +1,25 @@
 package com.example.sea_port_simulation.jahid_2211710;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class LogInpageViewController
 {
     @javafx.fxml.FXML
+    private TextField userIDtextfield;
+    @javafx.fxml.FXML
+    private TextField passwordtextfield;
+    @javafx.fxml.FXML
+    private ComboBox<String> userTypecombobox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        userTypecombobox.getItems().addAll("Port Administrator","Ship captain","client","Harbour pilot","cargo manager","custom clearance officer","security officer","accountant");
+
+    }
+
+    @javafx.fxml.FXML
+    public void logINButtonOnAction(ActionEvent actionEvent) {
+    }
+}
